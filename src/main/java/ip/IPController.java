9ebb75624759f93be693506f60bb4f9f1d77c2c6
Roman Protas <r.protas93@gmail.com>
2018-03-12 16:20:50 +0100

    Initial commit

--- src/main/java/ip/IPController.java
+++ src/main/java/ip/IPController.java
@@ -11,20 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class IPController {
 
+    private String remoteAddr, remoteHost, remoteUser;
+    private int remotePort;
+    private IpDTO ipDTO = new IpDTO();
+
     @RequestMapping("/getip")
     public @ResponseBody
-    IpDTO getIpJSON(HttpServletRequest request, HttpServletResponse response) {
+    IpDTO getIP(HttpServletRequest request, HttpServletResponse response) {
 
-        String remoteAddr = request.getRemoteAddr();
-        String remoteHost = request.getRemoteHost();
-        String remoteUser = request.getRemoteUser();
-        int remotePort = request.getRemotePort();
+        remoteAddr = request.getRemoteAddr();
+        remoteHost = request.getRemoteHost();
+        remoteUser = request.getRemoteUser();
+        remotePort = request.getRemotePort();
 
-        IpDTO ipDTO = new IpDTO();
         ipDTO.setRemoteAddr(remoteAddr);
         ipDTO.setRemotePort(remotePort);
         ipDTO.setRemoteHost(remoteHost);
         ipDTO.setRemoteUser(remoteUser);
+
         return ipDTO;
     }
 }
--- src/main/java/ip/IpDTO.java
+++ src/main/java/ip/IpDTO.java
@@ -2,41 +2,38 @@ package ip;
 
 public class IpDTO {
 
-    private String remoteAddr;
-    private String remoteHost;
+    private String remoteAddr, remoteHost, remoteUser;
     private int remotePort;
 
     public String getRemoteAddr() {
         return remoteAddr;
     }
 
+    public void setRemoteAddr(String remoteAddr) {
+        this.remoteAddr = remoteAddr;
+    }
+
     public String getRemoteHost() {
         return remoteHost;
     }
 
-    public int getRemotePort() {
-        return remotePort;
+    public void setRemoteHost(String remoteHost) {
+        this.remoteHost = remoteHost;
     }
 
     public String getRemoteUser() {
         return remoteUser;
     }
 
-    private String remoteUser;
-
-    public void setRemoteAddr(String remoteAddr) {
-        this.remoteAddr = remoteAddr;
-    }
-
-    public void setRemotePort(int remotePort) {
-        this.remotePort = remotePort;
-    }
-
     public void setRemoteUser(String remoteUser) {
         this.remoteUser = remoteUser;
     }
 
-    public void setRemoteHost(String remoteHost) {
-        this.remoteHost = remoteHost;
+    public int getRemotePort() {
+        return remotePort;
+    }
+
+    public void setRemotePort(int remotePort) {
+        this.remotePort = remotePort;
     }
 }
